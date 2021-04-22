f2d95edac12d21e29c05b90016321a309f4e9be6
Arianna Carotti <arianna.carotti@gmail.com>
2021-04-22 16:38:01 +0200

    andando avanti con la struttura

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            contacts: [
+                {
+                    nome: 'Michele',
+                    avatar: '_1'
+                },
+                {
+                    nome: 'Fabio',
+                    avatar: '_2'
+                },
+                {
+                    nome: 'Samuele',
+                    avatar: '_3'
+                },
+                {
+                    nome: 'Luisa',
+                    avatar: '_4'
+                }
+
+            ]
+        }, 
+    })   
